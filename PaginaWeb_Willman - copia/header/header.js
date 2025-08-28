34635dda02d57f8e42267cc9db9d7321717a7788
Jesuavi <jesus.delgado@correo.unimet.edu.ve>
2025-08-28 12:09:44 -0400

    ...

--- PaginaWeb_Willman - copia/header/header.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("../header/header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-container").innerHTML = data;
-        });
-});
